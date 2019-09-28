72d540deed8ae0ad5704332ef56c65fd2f92ef69
Nathan Hosselton <nathanhosselton@gmail.com>
2019-09-28 13:05:27 -0400

    Initial Commit

---
+++ VeoRide Excercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VeoRide Excercise
+//
+//  Created by Nathan Hosselton on 9/28/19.
+//  Copyright © 2019 Nathan Hosselton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
