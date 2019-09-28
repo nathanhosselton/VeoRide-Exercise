df8ea2edc0c10a4bd1524e1e66bd83ead8e01621
Nathan Hosselton <nathanhosselton@gmail.com>
2019-09-28 14:35:23 -0400

    Configure view and obtain coordinate from user tap
    - Add and constrain map view in Storyboard
    - Add and constrain Start button in Storyboard
    - Configure Start button appearance and state
    - Add tap gesture recognizer to map view
    - Connect outlets and actions for map view, start
    button, and tap gesture
    - Obtain coordinate of tapped location on
    map
    - Mark MapViewController `final`
    - Mark TODOs

--- VeoRide Excercise/Sources/MapViewController.swift
+++ VeoRide Excercise/Sources/MapViewController.swift
@@ -1,10 +1,40 @@
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+final class MapViewController: UIViewController {
+    @IBOutlet private weak var mapView: MKMapView!
+    @IBOutlet private weak var startButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //TODO: Get user's current location and update map region
     }
+
+    /// Handler for the map view's tap gesture recognizer, used to determine the location of the user's tap.
+    @IBAction private func onMapTapped(sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            let tappedPoint = sender.location(in: mapView)
+            let coordinate = mapView.convert(tappedPoint, toCoordinateFrom: mapView)
+
+            print(coordinate)
+
+            //TODO: Resolve location from tapped destination
+            //TODO: Determine and display route to tapped destination
+            //TODO: Enable Start button
+        }
+    }
+
+    /// Handler for the view's Start button, used for initiating navigation.
+    @IBAction private func onStartPressed() {
+        print("Start Pressed")
+
+        //TODO: Initiate navigation to tapped destination
+        //TODO: Convert to cancel button?
+    }
+
+}
+
+extension MapViewController: MKMapViewDelegate {
 
 }
